be9a0ad93888905240476e688ca11d1bb497ea63
seethinks <seethinks@gmail.com>
2020-05-02 00:45:21 +0800

    first sumbit

---
+++ lib/entity/k_line_entity.dart
@@ -0,0 +1,41 @@
+
+
+
+import '../entity/k_entity.dart';
+
+class KLineEntity extends KEntity {
+  double open;
+  double high;
+  double low;
+  double close;
+  double vol;
+  double amount;
+  int id;
+
+  KLineEntity.fromJson(Map<String, dynamic> json) {
+    open = (json['open'] as num)?.toDouble();
+    high = (json['high'] as num)?.toDouble();
+    low = (json['low'] as num)?.toDouble();
+    close = (json['close'] as num)?.toDouble();
+    vol = (json['vol'] as num)?.toDouble();
+    amount = (json['amount'] as num)?.toDouble();
+    id = (json['timeStamp'] as num)?.toInt();
+  }
+
+  Map<String, dynamic> toJson(){
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['open'] = this.open;
+    data['close'] = this.close;
+    data['high'] = this.high;
+    data['low'] = this.low;
+    data['vol'] = this.vol;
+    data['amount'] = this.amount;
+    return data;
+  }
+
+  @override
+  String toString() {
+    return 'MarketModel{open: $open, high: $high, low: $low, close: $close, vol: $vol, id: $id}';
+  }
+}
